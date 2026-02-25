41a92c619382e303b543cb3286de555b856330cf
Mariia Ilnitska <maria.ladesigner@gmail.com>
2026-02-25 22:49:58 +0000

    generator_v1.py

--- v1_console:/generator_v1.py
+++ v1_console:/generator_v1.py
@@ -1,20 +1,17 @@
-import random  # imports the random module for generating random characters
-import string  # imports the string module which contains ready-made character sets
+import random  
+import string  
 
-def generate_password(length=12): # function generates a password, def length is 12
+def generate_password(length=12): 
 
-  letters = string.ascii_letters  # all uppercase + lowercase English letters
-  digits = string.digits          # numbers 0–9
-  symbols = string.punctuation    # special characters like !@#$%^&*
+  letters = string.ascii_letters  
+  digits = string.digits         
+  symbols = string.punctuation   
 
-  all_chars = letters + digits + symbols # combine all character sets into one string
+  all_chars = letters + digits + symbols 
 
   password = ''.join(random.choice(all_chars) for _ in range(length)) 
-# random.choice() picks 1 random character each time
-# the generator runs "length" times
-# join() glues all chosen characters into one final password string
 
-  return password  # return the final generated password
+  return password  
 
-user_length = int(input("Password length: ")) # ask the user for the length and convert to integer
-print("Your password:", generate_password(user_length))  # call the function with user input and print result
+user_length = int(input("Password length: ")) 
+print("Your password:", generate_password(user_length))  
